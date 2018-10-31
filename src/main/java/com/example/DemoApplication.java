1042e08230a73a93675a5c73aa33cbb5905b88d5
Rob Winch <rwinch@users.noreply.github.com>
2018-10-30 21:46:36 -0500

    Exclude security auto config

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,11 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
 /**
  * @author Rob Winch
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveSecurityAutoConfiguration.class)
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -1,36 +1,24 @@
 package com.example;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.context.SecurityContextServerWebExchangeWebFilter;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
-import reactor.core.publisher.Mono;
+import org.springframework.web.server.WebFilter;
 
 
 /**
  * @author Rob Winch
  */
-@EnableWebFluxSecurity
+@Configuration
 public class SecurityConfig {
-	@Bean
-	SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-		return http
-				.securityMatcher(e -> ServerWebExchangeMatcher.MatchResult.notMatch())
-				.csrf().disable()
-				.addFilterAt(simpleFilter(), SecurityWebFiltersOrder.AUTHENTICATION)
-				.authorizeExchange()
-					.anyExchange().authenticated()
-					.and()
-				.build();
-	}
 
 	@Bean
 	SecurityContextServerWebExchangeWebFilter reactorContextWebFilter() {
@@ -39,16 +27,9 @@ public class SecurityConfig {
 
 	@Bean
 	@Order(0)
-	AuthenticationWebFilter simpleFilter() {
-		AuthenticationWebFilter result = new AuthenticationWebFilter(new NoopAuthenticationManager());
-		result.setAuthenticationConverter(e -> Mono.just(new TestingAuthenticationToken("the-username", "p", "ROLE_USER")));
-		return result;
-	}
-
-	static class NoopAuthenticationManager implements ReactiveAuthenticationManager {
-		@Override
-		public Mono<Authentication> authenticate(Authentication authentication) {
-			return Mono.just(authentication);
-		}
+	WebFilter simpleFilter() {
+		return (e,c) ->  c
+			.filter(e)
+			.subscriberContext(ReactiveSecurityContextHolder.withAuthentication(new TestingAuthenticationToken("the-username", "p", "USER")));
 	}
 }
