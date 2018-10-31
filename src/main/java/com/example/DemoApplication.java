5d89bc2164127125e5ec44550aa7470e227eaa8e
Rob Winch <rwinch@users.noreply.github.com>
2018-10-30 22:28:25 -0500

    Polish

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,12 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
 /**
  * @author Rob Winch
  */
-@SpringBootApplication(exclude = ReactiveSecurityAutoConfiguration.class)
+@SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.ServerWebExchangeDecorator;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
@@ -17,38 +16,31 @@ import java.security.Principal;
 @Configuration
 public class SecurityConfig {
 
+	/**
+	 * Overrides the {@link ServerWebExchange#getPrincipal()} from the {@link Context}
+	 * @return
+	 */
 	@Bean
 	WebFilter reactorContextWebFilter() {
 		return (e,c) ->  {
 			Mono<Principal> principal = Mono.subscriberContext()
 					.filter(ctx -> ctx.hasKey(Principal.class))
 					.flatMap(ctx -> ctx.<Mono<Principal>>get(Principal.class));
-			return c.filter(new SecurityContextServerWebExchange(e, principal));
+			return c.filter(e.mutate().principal(principal).build());
 		};
 	}
 
+	/**
+	 * Authenticates the user as "the-username" by setting the {@link Context}. This is
+	 * executed before {@link #reactorContextWebFilter()}
+	 * @return
+	 */
 	@Bean
 	@Order(0)
-	WebFilter simpleFilter() {
+	WebFilter authenticationFilter() {
 		Principal p = () -> "the-username";
 		return (e,c) ->  c
 			.filter(e)
 			.subscriberContext(Context.of(Principal.class, Mono.just(p)));
-	}
-
-	static class SecurityContextServerWebExchange extends
-			ServerWebExchangeDecorator {
-		private final Mono<Principal> context;
-
-		public SecurityContextServerWebExchange(ServerWebExchange delegate, Mono<Principal> context) {
-			super(delegate);
-			this.context = context;
-		}
-
-		@Override
-		@SuppressWarnings("unchecked")
-		public <T extends Principal> Mono<T> getPrincipal() {
-			return (Mono<T>) this.context;
-		}
 	}
 }
